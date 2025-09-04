ee33f1ac5fe303608255125c180980f12cbc93ae
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-04 00:08:38 -0700

    Update hashMapOneLoop.py

--- DSA_Practice/Blind-75/Arrays & Hashing/Two Sum/hashMapOneLoop.py
+++ DSA_Practice/Blind-75/Arrays & Hashing/Two Sum/hashMapOneLoop.py
@@ -1,6 +1,8 @@
-sum = {}
-for i, n in enumerate(nums):
-    diff = target - n
-    if diff in sum:
-        return [i, sum[diff]]
-    sum[n] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sum = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in sum:
+                return [i, sum[diff]]
+            sum[n] = i
