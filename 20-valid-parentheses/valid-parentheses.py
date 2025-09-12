0faae83710cbea564a9e304fe5e2b1a2e62714e8
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-12 08:46:49 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (82.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,7 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while '()' in s or '{}' in s or '[]' in s:
-            s = s.replace('()', '')
-            s = s.replace('{}', '')
-            s = s.replace('[]', '')
-        return s == ''
+
+        # Brute Force Approach
+
+        # while '()' in s or '{}' in s or '[]' in s:
+        #     s = s.replace('()', '')
+        #     s = s.replace('{}', '')
+        #     s = s.replace('[]', '')
+        # return s == ''
+
+        # Stack
+
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
