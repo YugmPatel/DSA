d44911bde70078149b37e496ac5de02e2ab589e9
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-13 11:51:26 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (66.76%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -40,30 +40,52 @@ class Solution:
 
         # Binary Seach with Two Pass
 
+        # l, r = 0, len(nums) - 1
+
+        # while l < r:
+        #     m = (l + r) // 2
+        #     if nums[m] > nums[r]:
+        #         l = m + 1
+        #     else:
+        #         r = m
+
+        # pivot = l
+        # l, r = 0, len(nums) - 1
+
+        # if target >= nums[pivot] and target <= nums[r]:
+        #     l = pivot
+        # else:
+        #     r = pivot - 1
+
+        # while l <= r:
+        #     m = (l + r) // 2
+        #     if nums[m] == target:
+        #         return m
+        #     elif nums[m] < target:
+        #         l = m + 1
+        #     else:
+        #         r = m - 1
+
+        # return -1
+
+        # Binary Seach with One Pass
+
         l, r = 0, len(nums) - 1
-
-        while l < r:
-            m = (l + r) // 2
-            if nums[m] > nums[r]:
-                l = m + 1
-            else:
-                r = m
-
-        pivot = l
-        l, r = 0, len(nums) - 1
-
-        if target >= nums[pivot] and target <= nums[r]:
-            l = pivot
-        else:
-            r = pivot - 1
 
         while l <= r:
-            m = (l + r) // 2
-            if nums[m] == target:
-                return m
-            elif nums[m] < target:
-                l = m + 1
-            else:
-                r = m - 1
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
 
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
         return -1
