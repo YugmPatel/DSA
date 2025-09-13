da4b4f5912cc0f60357aac86541da7c2f32870cc
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-13 11:45:04 -0700

    Time: 0 ms (100.00%) | Memory: 18.2 MB (30.98%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -6,18 +6,29 @@ class Solution:
 
         # Binary Search
 
-        result = nums[0]
+        # result = nums[0]
+        # left, right = 0, len(nums) - 1
+
+        # while left <= right:
+        #     if nums[left] < nums[right]:
+        #         result = min(res, nums[left])
+        #         break
+
+        #     m = (left + right) // 2
+        #     result = min(result, nums[m])
+        #     if nums[m] >= nums[left]:
+        #         left = m + 1
+        #     else:
+        #         right = m - 1
+        # return result
+
+        # Binary Search with Lower Bound
+
         left, right = 0, len(nums) - 1
-
-        while left <= right:
-            if nums[left] < nums[right]:
-                result = min(res, nums[left])
-                break
-
-            m = (left + right) // 2
-            result = min(result, nums[m])
-            if nums[m] >= nums[left]:
-                left = m + 1
+        while left < right:
+            m = left + (right - left) // 2
+            if nums[m] < nums[right]:
+                right = m
             else:
-                right = m - 1
-        return result
+                left = m + 1
+        return nums[left]
