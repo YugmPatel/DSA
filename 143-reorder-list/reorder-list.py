5fd3f40d613e5d8bd2891ae9433027b28378bf26
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-17 10:55:43 -0700

    Time: 0 ms (100.00%) | Memory: 23.2 MB (80.92%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,26 +5,50 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
+        # Brute Force Approach
+
+        if not head:
+            return
+
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur)
+            cur = cur.next
+
+        i, j = 0, len(nodes) - 1
+        while i < j:
+            nodes[i].next = nodes[j]
+            i += 1
+            if i >= j:
+                break
+            nodes[j].next = nodes[i]
+            j -= 1
+
+        nodes[i].next = None
+        
+        
+        
         # Slow and Fast Pointer
 
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        # slow, fast = head, head.next
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
 
-        second = slow.next
-        prev = slow.next = None
-        while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
+        # second = slow.next
+        # prev = slow.next = None
+        # while second:
+        #     temp = second.next
+        #     second.next = prev
+        #     prev = second
+        #     second = temp
         
-        first, second = head, prev
-        while second: 
-            temp1, temp2 = first.next, second.next
-            first.next = second
-            second.next = temp1
-            first, second = temp1, temp2
+        # first, second = head, prev
+        # while second: 
+        #     temp1, temp2 = first.next, second.next
+        #     first.next = second
+        #     second.next = temp1
+        #     first, second = temp1, temp2
 
         
