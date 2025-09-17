5d0d895f56f2302c2048b32a6e2df723622c1ee2
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-17 10:57:30 -0700

    Time: 7 ms (20.05%) | Memory: 27.1 MB (5.17%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -7,26 +7,47 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         # Brute Force Approach
 
-        if not head:
-            return
+        # if not head:
+        #     return
 
-        nodes = []
-        cur = head
-        while cur:
-            nodes.append(cur)
-            cur = cur.next
+        # nodes = []
+        # cur = head
+        # while cur:
+        #     nodes.append(cur)
+        #     cur = cur.next
 
-        i, j = 0, len(nodes) - 1
-        while i < j:
-            nodes[i].next = nodes[j]
-            i += 1
-            if i >= j:
-                break
-            nodes[j].next = nodes[i]
-            j -= 1
+        # i, j = 0, len(nodes) - 1
+        # while i < j:
+        #     nodes[i].next = nodes[j]
+        #     i += 1
+        #     if i >= j:
+        #         break
+        #     nodes[j].next = nodes[i]
+        #     j -= 1
 
-        nodes[i].next = None
+        # nodes[i].next = None
         
+        # Recursion
+
+        def rec(root: ListNode, cur: ListNode) -> ListNode:
+            if not cur:
+                return root
+
+            root = rec(root, cur.next)
+            if not root:
+                return None
+
+            tmp = None
+            if root == cur or root.next == cur:
+                cur.next = None
+            else:
+                tmp = root.next
+                root.next = cur
+                cur.next = tmp
+
+            return tmp
+
+        head = rec(head, head.next)
         
         
         # Slow and Fast Pointer
