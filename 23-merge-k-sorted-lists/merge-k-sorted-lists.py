7cea403529e935884bf5fe2ccbd534f256e54e61
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-18 09:58:31 -0700

    Time: 1113 ms (14.03%) | Memory: 20.1 MB (69.99%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,16 +5,68 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        nodes = []
-        for lst in lists:
-            while lst:
-                nodes.append(lst.val)
-                lst = lst.next
-        nodes.sort()
 
-        res = ListNode(0)
-        cur = res
-        for node in nodes:
-            cur.next = ListNode(node)
-            cur = cur.next
-        return res.next
+        # Brute Force Approach
+
+        # nodes = []
+        # for lst in lists:
+        #     while lst:
+        #         nodes.append(lst.val)
+        #         lst = lst.next
+        # nodes.sort()
+
+        # res = ListNode(0)
+        # cur = res
+        # for node in nodes:
+        #     cur.next = ListNode(node)
+        #     cur = cur.next
+        # return res.next
+
+        # Iteration
+
+        # res = ListNode(0)
+        # cur = res
+
+        # while True:
+        #     minNode = -1
+        #     for i in range(len(lists)):
+        #         if not lists[i]:
+        #             continue
+        #         if minNode == -1 or lists[minNode].val > lists[i].val:
+        #             minNode = i
+
+        #     if minNode == -1:
+        #         break
+        #     cur.next = lists[minNode]
+        #     lists[minNode] = lists[minNode].next
+        #     cur = cur.next
+
+        # return res.next
+
+        # Merge list one by one
+
+        if len(lists) == 0:
+            return None
+
+        for i in range(1, len(lists)):
+            lists[i] = self.mergeList(lists[i - 1], lists[i])
+
+        return lists[-1]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
