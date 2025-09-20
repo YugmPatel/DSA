2d5f17d7cd14cae8f8e06fcd932827f667b9797b
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-19 17:43:21 -0700

    Time: 18 ms (26.20%) | Memory: 20.3 MB (36.79%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,8 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
         # Brute Force Approach
 
@@ -45,28 +45,56 @@ class Solution:
 
         # Merge list one by one
 
+    #     if len(lists) == 0:
+    #         return None
+
+    #     for i in range(1, len(lists)):
+    #         lists[i] = self.mergeList(lists[i - 1], lists[i])
+
+    #     return lists[-1]
+
+    # def mergeList(self, l1, l2):
+    #     dummy = ListNode()
+    #     tail = dummy
+
+    #     while l1 and l2:
+    #         if l1.val < l2.val:
+    #             tail.next = l1
+    #             l1 = l1.next
+    #         else:
+    #             tail.next = l2
+    #             l2 = l2.next
+    #         tail = tail.next
+    #     if l1:
+    #         tail.next = l1
+    #     if l2:
+    #         tail.next = l2
+    #     return dummy.next
+
+class NodeWrapper:
+    def __init__(self, node):
+        self.node = node
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if len(lists) == 0:
             return None
 
-        for i in range(1, len(lists)):
-            lists[i] = self.mergeList(lists[i - 1], lists[i])
+        res = ListNode(0)
+        cur = res
+        minHeap = []
 
-        return lists[-1]
+        for lst in lists:
+            if lst is not None:
+                heapq.heappush(minHeap, NodeWrapper(lst))
 
-    def mergeList(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
+        while minHeap:
+            node_wrapper = heapq.heappop(minHeap)
+            cur.next = node_wrapper.node
+            cur = cur.next
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
-        return dummy.next
+            if node_wrapper.node.next:
+                heapq.heappush(minHeap, NodeWrapper(node_wrapper.node.next))
+
+        return res.next
