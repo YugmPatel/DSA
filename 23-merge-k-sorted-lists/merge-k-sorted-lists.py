ff9ad655daf66a1b232f82abe57044e9a6cdeb7c
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-19 18:12:22 -0700

    Time: 12 ms (46.49%) | Memory: 20.1 MB (89.76%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -103,38 +103,70 @@ class Solution:
 
     # Recursion
 
+    #     if not lists or len(lists) == 0:
+    #         return None
+    #     return self.divide(lists, 0, len(lists) - 1)
+
+    # def divide(self, lists, l, r):
+    #     if l > r:
+    #         return None
+    #     if l == r:
+    #         return lists[l]
+
+    #     mid = l + (r - l) // 2
+    #     left = self.divide(lists, l, mid)
+    #     right = self.divide(lists, mid + 1, r)
+
+    #     return self.conquer(left, right)
+
+    # def conquer(self, l1, l2):
+    #     dummy = ListNode(0)
+    #     curr = dummy
+
+    #     while l1 and l2:
+    #         if l1.val <= l2.val:
+    #             curr.next = l1
+    #             l1 = l1.next
+    #         else:
+    #             curr.next = l2
+    #             l2 = l2.next
+    #         curr = curr.next
+
+    #     if l1:
+    #         curr.next = l1
+    #     else:
+    #         curr.next = l2
+
+    #     return dummy.next
+
         if not lists or len(lists) == 0:
             return None
-        return self.divide(lists, 0, len(lists) - 1)
 
-    def divide(self, lists, l, r):
-        if l > r:
-            return None
-        if l == r:
-            return lists[l]
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
 
-        mid = l + (r - l) // 2
-        left = self.divide(lists, l, mid)
-        right = self.divide(lists, mid + 1, r)
-
-        return self.conquer(left, right)
-
-    def conquer(self, l1, l2):
-        dummy = ListNode(0)
-        curr = dummy
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
 
         while l1 and l2:
-            if l1.val <= l2.val:
-                curr.next = l1
+            if l1.val < l2.val:
+                tail.next = l1
                 l1 = l1.next
             else:
-                curr.next = l2
+                tail.next = l2
                 l2 = l2.next
-            curr = curr.next
+            tail = tail.next
 
         if l1:
-            curr.next = l1
-        else:
-            curr.next = l2
+            tail.next = l1
+        if l2:
+            tail.next = l2
 
         return dummy.next
