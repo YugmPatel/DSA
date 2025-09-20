7924d879e87a05984c0abd9427fdf1251db284b4
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-20 10:13:23 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.37%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -8,6 +8,7 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
 
         # Breadth First Search
+
         # if not root:
         #     return None
         # queue = deque([root])
@@ -22,11 +23,25 @@ class Solution:
 
         # Depth First Search
 
-        if not root: return None
+        # if not root: return None
 
-        root.left, root.right = root.right, root.left
+        # root.left, root.right = root.right, root.left
 
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        # self.invertTree(root.left)
+        # self.invertTree(root.right)
 
+        # return root
+
+        # Iterative DFS
+
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
         return root
