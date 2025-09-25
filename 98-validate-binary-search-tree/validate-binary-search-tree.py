d001279d016297c3be30e1070159674786b5c559
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-25 16:57:21 -0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (29.98%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,6 +6,9 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        # DFS
+
         def valid(node, left, right):
             if not node:
                 return True
@@ -17,4 +20,22 @@ class Solution:
             )
 
         return valid(root, float("-inf"), float("inf"))
+
+        # BFS
+
+        if not root:
+            return True
+
+        q = deque([(root, float("-inf"), float("inf"))])
+
+        while q:
+            node, left, right = q.popleft()
+            if not (left < node.val < right):
+                return False
+            if node.left:
+                q.append((node.left, left, node.val))
+            if node.right:
+                q.append((node.right, node.val, right))
+
+        return True
         
