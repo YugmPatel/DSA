faef4b6f3d374e48225486dfca8c2710ea26dd1c
YugmPatel <141047110+YugmPatel@users.noreply.github.com>
2025-09-25 17:02:54 -0700

    Time: 0 ms (100.00%) | Memory: 19.9 MB (29.98%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -5,28 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
-
-    left_check = staticmethod(lambda val, limit: val < limit)
-    right_check = staticmethod(lambda val, limit: val > limit)
-
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
 
-        if (not self.isValid(root.left, root.val, self.left_check) or
-            not self.isValid(root.right, root.val, self.right_check)):
-            return False
+    # left_check = staticmethod(lambda val, limit: val < limit)
+    # right_check = staticmethod(lambda val, limit: val > limit)
 
-        return self.isValidBST(root.left) and self.isValidBST(root.right)
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    #     if not root:
+    #         return True
 
-    def isValid(self, root: Optional[TreeNode], limit: int, check) -> bool:
-        if not root:
-            return True
-        if not check(root.val, limit):
-            return False
-        return (self.isValid(root.left, limit, check) and
-                self.isValid(root.right, limit, check))
+    #     if (not self.isValid(root.left, root.val, self.left_check) or
+    #         not self.isValid(root.right, root.val, self.right_check)):
+    #         return False
+
+    #     return self.isValidBST(root.left) and self.isValidBST(root.right)
+
+    # def isValid(self, root: Optional[TreeNode], limit: int, check) -> bool:
+    #     if not root:
+    #         return True
+    #     if not check(root.val, limit):
+    #         return False
+    #     return (self.isValid(root.left, limit, check) and
+    #             self.isValid(root.right, limit, check))
 
         # DFS
 
@@ -44,19 +44,19 @@ class Solution:
 
         # BFS
 
-        # if not root:
-        #     return True
+        if not root:
+            return True
 
-        # q = deque([(root, float("-inf"), float("inf"))])
+        q = deque([(root, float("-inf"), float("inf"))])
 
-        # while q:
-        #     node, left, right = q.popleft()
-        #     if not (left < node.val < right):
-        #         return False
-        #     if node.left:
-        #         q.append((node.left, left, node.val))
-        #     if node.right:
-        #         q.append((node.right, node.val, right))
+        while q:
+            node, left, right = q.popleft()
+            if not (left < node.val < right):
+                return False
+            if node.left:
+                q.append((node.left, left, node.val))
+            if node.right:
+                q.append((node.right, node.val, right))
 
-        # return True
+        return True
         
