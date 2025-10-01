de50397bba94163eb82a596b3114c2c1555c2f11
Yugm Patel <141047110+YugmPatel@users.noreply.github.com>
2025-09-30 21:02:12 -0700

    Time: 87 ms (50.69%) | Memory: 21 MB (89.96%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -5,37 +5,76 @@
 #         self.left = None
 #         self.right = None
 
+# DFS
+
+# class Codec:
+
+#     def serialize(self, root):
+#         res = []
+
+#         def dfs(node):
+#             if not node:
+#                 res.append("N")
+#                 return
+#             res.append(str(node.val))
+#             dfs(node.left)
+#             dfs(node.right)
+#         dfs(root)
+#         return ",".join(res)
+        
+
+#     def deserialize(self, data):
+#         vals = data.split(",")
+#         self.i = 0
+
+#         def dfs():
+#             if vals[self.i] == "N":
+#                 self.i += 1
+#                 return None
+#             node = TreeNode(int(vals[self.i]))
+#             self.i += 1
+#             node.left = dfs()
+#             node.right = dfs()
+#             return node
+#         return dfs()
+
 class Codec:
 
     def serialize(self, root):
+        if not root:
+            return "N"
         res = []
-
-        def dfs(node):
+        queue = deque([root])
+        while  queue:
+            node = queue.popleft()
             if not node:
                 res.append("N")
-                return
-            res.append(str(node.val))
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
+            else:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
         return ",".join(res)
-        
+
 
     def deserialize(self, data):
         vals = data.split(",")
-        self.i = 0
+        if vals[0] == "N":
+            return None
+        root = TreeNode(int(vals[0]))
+        queue = deque([root])
+        index = 1
+        while queue:
+            node = queue.popleft()
+            if vals[index] != "N":
+                node.left = TreeNode(int(vals[index]))
+                queue.append(node.left)
+            index += 1
 
-        def dfs():
-            if vals[self.i] == "N":
-                self.i += 1
-                return None
-            node = TreeNode(int(vals[self.i]))
-            self.i += 1
-            node.left = dfs()
-            node.right = dfs()
-            return node
-        return dfs()
-        
+            if vals[index] != "N":
+                node.right = TreeNode(int(vals[index]))
+                queue.append(node.right)
+            index += 1
+        return root
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
