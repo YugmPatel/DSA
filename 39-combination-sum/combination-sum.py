d2569c7c08e55095b4c1b054bb844d328bda51aa
Yugm Patel <141047110+YugmPatel@users.noreply.github.com>
2025-10-16 19:43:59 -0700

    Time: 4 ms (89.60%) | Memory: 17.8 MB (87.47%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,39 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        # Backtracking
+        # result = []
+
+        # def dfs(i, curr, total):
+        #     if total == target:
+        #         result.append(curr.copy())
+        #         return
+        #     if i >= len(candidates) or total > target:
+        #         return
+
+        #     curr.append(candidates[i])
+        #     dfs(i, curr, total + candidates[i])
+        #     curr.pop()
+        #     dfs(i + 1, curr, total)
+
+        # dfs(0, [], 0)
+        # return result
+        
+        # Backtracking Optimal
+
         result = []
+        candidates.sort()
 
         def dfs(i, curr, total):
             if total == target:
                 result.append(curr.copy())
                 return
-            if i >= len(candidates) or total > target:
-                return
-
-            curr.append(candidates[i])
-            dfs(i, curr, total + candidates[i])
-            curr.pop()
-            dfs(i + 1, curr, total)
-
+            
+            for j in range(i, len(candidates)):
+                if total + candidates[j] > target:
+                    return
+                curr.append(candidates[j])
+                dfs(j, curr, total + candidates[j])
+                curr.pop()
         dfs(0, [], 0)
         return result
-        
