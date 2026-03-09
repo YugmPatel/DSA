8c984a647611e7d6ceddf1b39f423e2d12026139
Yugm Patel <141047110+YugmPatel@users.noreply.github.com>
2026-03-08 21:40:55 -0700

    Time: 31 ms (88.09%) | Memory: 28.6 MB (67.95%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,26 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # max_profit = float('-inf')
 
-        # Brute Force Approach
-
-        # result = 0
         # for i in range(len(prices)):
-        #     buy = prices[i]
         #     for j in range(i + 1, len(prices)):
-        #         sell  = prices[j]
-        #         result = max(result, sell - buy)
-        # return result
+        #         profit = prices[j] - prices[i]
 
-        # Two Pointers
+        #         if profit > 0:
+        #             max_profit = max(max_profit, profit)
+        # return max_profit if max_profit > float('-inf') else 0
 
-        left, right = 0, 1
-        maxP = 0
+        # Time: O(n^2)
+        # Space: O(1)
 
-        while right < len(prices):
-            if prices[left] < prices[right]:
-                profit = prices[right] - prices[left]
-                maxP = max(maxP, profit)
-            else:
-                left = right
-            right += 1
-        return maxP
+        min_price = float('inf')
+        max_price = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            
+            prices = price - min_price
+            if prices > max_price:
+                max_price = prices
+        return max_price
+        
