52a047c7460e27036f310ec9a72a9137506805c9
Yugm Patel <141047110+YugmPatel@users.noreply.github.com>
2026-03-21 14:59:42 -0700

    Time: 19 ms (22.34%) | Memory: 23.9 MB (32.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,7 +2,24 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = defaultdict(list)
+        # Brute Force Approach
+        # ans = defaultdict(list)
+        # for s in strs:
+        #     ans[tuple(sorted(s))].append(s)
+        # return list(ans.values())
+        # Time: O(N*K*logK) where N is length of strs and K is maximum length of string in N. So, first loop will take O(N) and O(KlogK) because we sorted s as well
+
+        # Space: O(N*K)
+
+
+        # Optimized Approach
+
+        anagram_dict = defaultdict(list)
+        
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return list(ans.values())
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            key = tuple(count)
+            anagram_dict[key].append(s)
+        return list(anagram_dict.values())
